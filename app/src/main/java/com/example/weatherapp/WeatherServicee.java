e5f726ffc70778c4082cc9dc4d2e13c857fba5fe
Sanduniphgr <e2145322@bit.uom.lk>
2024-06-05 17:45:19 +0530

    repo_update

---
+++ app/src/main/java/com/example/weatherapp/WeatherServicee.java
@@ -0,0 +1,10 @@
+package com.example.weatherapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherServicee {
+    @GET("weather")
+    Call<WeatherResponsee> getWeather(@Query("lat") double latitude, @Query("lon") double longitude, @Query("appid") String apiKey);
+}
